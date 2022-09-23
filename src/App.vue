4153c304bc52416604eadbf514556cbe1616965b
haiou <3529591183@qq.com>
2022-09-23 20:17:38 +0800

    ...

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <myTest v-if="show"></myTest>
+  </div>
+</template>
+<script>
+import myTest from "./components/v-model和.sync/index.vue";
+export default {
+  components: {
+    myTest,
+  },
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+<style scoped>
+.app {
+  width: 900px;
+}
+</style>
---
+++ src/components/v-model和.sync/addsub.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button>-</button>
+    {{ value111 }}
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VuePractiseAddsub",
+  props: {
+    value111: {
+      type: Number,
+      default: 0,
+    },
+  },
+  // model: {
+  //   prop: "value111",
+  //   event: "change111",
+  // },
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {
+    add() {
+      this.$emit("update:value111", this.value111 + 1);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
